4c80a1957dcd5b4395c27ec98378987f240c1497
Oscar Callisaya <oscar.callisaya@niceincontact.com>
2022-06-12 14:20:19 -0400

    3. Using Encoder, values not saved in memory, directly send to buffer.

--- data/products.go
+++ data/products.go
@@ -1,6 +1,10 @@
 package data
 
-import "time"
+import (
+	"encoding/json"
+	"io"
+	"time"
+)
 
 // Product defines the structure for an API product
 // https://pkg.go.dev/encoding/json#Marshal definition for JSON Marshal
@@ -15,7 +19,14 @@ type Product struct {
 	DeletedOn   string  `json:"-"`
 }
 
-func GetProducts() []*Product {
+type Products []*Product
+
+func (p *Products) ToJSON(w io.Writer) error {
+	e := json.NewEncoder(w)
+	return e.Encode(p)
+}
+
+func GetProducts() Products {
 	return productList
 }
 
--- handlers/products.go
+++ handlers/products.go
@@ -1,7 +1,6 @@
 package handlers
 
 import (
-	"encoding/json"
 	"log"
 	"net/http"
 	"working/data"
@@ -17,10 +16,8 @@ func NewProduct(l *log.Logger) *Products {
 
 func (p *Products) ServeHTTP(rw http.ResponseWriter, rh *http.Request) {
 	lp := data.GetProducts()
-	d, err := json.Marshal(lp)
+	err := lp.ToJSON(rw)
 	if err != nil {
 		http.Error(rw, "Unable to marshal json", http.StatusInternalServerError)
 	}
-
-	rw.Write(d)
 }
